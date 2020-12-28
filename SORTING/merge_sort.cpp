ad80176eb435294e044db548b5a11e092b0991d1
SumeetSanwal <63290497+SumeetSanwal@users.noreply.github.com>
2020-12-29 05:15:39 +0530

    changed temp array size

--- SORTING/merge_sort.cpp
+++ SORTING/merge_sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void merge(int a[],int l,int m, int r){
     int i=l,j=m+1,k=0;
-    int b[r-l];
+    int b[r-l+1];
     while(i<=m && j<=r){
         if(a[i]<=a[j]){
             b[k]=a[i];
@@ -36,7 +36,7 @@ void merge(int a[],int l,int m, int r){
 void mergesort(int a[],int l,int r){
    if(l>=r)
    return ;
-   int m=(l+r-1)/2;
+   int m=(l+r)/2;
    mergesort(a,l,m);
    mergesort(a,m+1,r);
    merge(a,l,m,r);
